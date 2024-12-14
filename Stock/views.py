a2cd260b8546f6bc6dab6f41a213f807809fec60
yongminkim0501 <yongmingim166@gmail.com>
2024-12-14 21:55:39 +0900

    로그인 기능 구현

--- Stock/views.py
+++ Stock/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'Stock/register.html', {'form': form})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # 로그인 후 이동할 페이지
+        else:
+            return render(request, 'Stock/login.html', {'error': '아이디 또는 비밀번호가 잘못되었습니다.'})
+    return render(request, 'Stock/login.html')
+
+@login_required
+def home(request):
+    return render(request, 'Stock/home.html', {'user': request.user})
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Stock'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
