29fd15e19d0194cf5398cacd920a9c4f3d2237f1
yongminkim0501 <yongmingim166@gmail.com>
2024-12-16 16:40:01 +0900

    Connect postgreSQL

--- config/settings.py
+++ config/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'usermanagedb',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
